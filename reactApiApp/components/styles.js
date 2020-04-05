b4b9290a3f7fba4907b3e35faa53f86a6c9405fa
Akash Pathak <pathakvikash9211@gamil.com>
2020-04-05 18:17:58 +0530

    added home class and styleling

---
+++ reactApiApp/components/styles.js
@@ -0,0 +1,27 @@
+const styles = StyleSheet.create({
+  homeDiv: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+  },
+  homePageImage: {
+    width: '75%',
+    height: '50%',
+  },
+  header: {
+    fontSize: 30,
+    color: 'orange',
+    textAlign: 'center',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'Heiti SC',
+      },
+      android: {
+        fontFamily: 'monospace',
+      },
+    }),
+  },
+});
+//Export your styles
+export default styles;
