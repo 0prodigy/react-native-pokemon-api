9d7319ce6fce7e25b768b1e8ff5031e646caf7b7
Akash Pathak <pathakvikash9211@gamil.com>
2020-04-05 17:16:48 +0530

    intialize pokelist

---
+++ reactApiApp/components/PokeList/index.js
@@ -0,0 +1,42 @@
+
+import React, { PureComponent } from 'react';
+import { View, Image, FlatList, TouchableOpacity, Text, ActivityIndicator } from 'react-native';
+import styles from './styles';
+
+export default class PokeList extends PureComponent {
+    state = {
+        pokeList: [],
+        loading: true
+    }
+    async componentDidMount() {
+        try {
+            const pokemonApiCall = await fetch('https://pokeapi.co/api/v2/pokemon/');
+            const pokemon = await pokemonApiCall.json();
+            this.setState({ pokeList: pokemon.results, loading: false });
+        } catch (err) {
+            console.log("Error fetching data-----------", err);
+        }
+    }
+
+    renderItem(data) {
+        return <TouchableOpacity style={{ backgroundColor: 'transparent' }}>
+            <View style={styles.listItemContainer}>
+                <Text style={styles.pokeItemHeader}>{data.item.name}</Text>
+                <Image source={{ uri: 'https://res.cloudinary.com/aa1997/image/upload/v1535930682/pokeball-image.jpg' }}
+                    style={styles.pokeImage} />
+            </View>
+        </TouchableOpacity>
+    }
+    render() {
+        const { pokeList, loading } = this.state;.
+        if (!loading) {
+            return <FlatList
+                data={pokeList}
+                renderItem={this.renderItem}
+                keyExtractor={(item) => item.name}
+            />
+        } else {
+            return <ActivityIndicator />
+        }
+    }
+}
